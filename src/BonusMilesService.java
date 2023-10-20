483dc82df96a6f7010257457f16bdb09624a21c6
ruslan <ruslan.st1982@gmail.com>
2023-10-20 12:39:58 +0300

    New commit

--- src/BonusMilesService.java
+++ src/BonusMilesService.java
@@ -1,8 +1,8 @@
 public class BonusMilesService {
     public int calculate(int miles) {
         int result;
-        int price = 20000;
-        miles = 70_000 / 20;
+        int price = 10000;
+        miles = 10_000 / 20;
         {
 
             result = (miles);
